0ba4481dc1d2728f55f1e51da8f0fb3433db71e8
Jeff Huang <jeffshuang2@gmail.com>
2020-01-20 17:11:57 -0800

    App now successfully scrapes all required information to the database: headline, article url, summary

--- server.js
+++ server.js
@@ -30,28 +30,45 @@ app.get("/scrape", function(req, res) {
     axios.get("https://www.seattletimes.com/").then(function(response) {
         let $ = cheerio.load(response.data);
 
-        console.log($);
-        console.log(response);
+        // console.log($);
+        // console.log(response);
 
-        $("li").each(function(i, element) {
+        $("li.top-stories-list-item").each(function(i, element) {
             let result = {};
+            
 
-            result.title = $(this)
+            result.summary = $(this)
                 .children("a")
-                .text();
+                .text().trim();
             result.link = $(this)
                 .children("a")
-                .attr("href");
+                .attr("href")
+                
+            result.headline = axios.get(result.link).then(function(res) {
+                console.log("RESULT.LINK IS HERE", result.link)
+                        let stuff = cheerio.load(res.data);
+                        // console.log("this is stuff ----------------", res.data);
+                    
+                        
+                    
+                        stuff("h1.article-title").each(function(i, element) {
+                            result.headline = stuff(this).text().trim();
+                            // console.log("THIS IS RESULT HEADLINE TWO: -------------- ", result.headline, "END");
+                            console.log("THIS SHOULD BE FINAL RESULT OBJECT " + JSON.stringify(result));
+                            
+                        })
 
+                        db.Article.create(result)
+                            .then(function(dbArticle) {
+                                console.log(dbArticle);
+                            })
+                            .catch(function(err) {
+                                console.log(err);
+                            });
+
+                    })
             
-            db.Article.create(result)
-                .then(function(dbArticle) {
-                    console.log(dbArticle);
-                })
-                .catch(function(err) {
-                    console.log(err);
-                });
-        });
+        })
         res.send("Scrape Complete");
     });
 });
